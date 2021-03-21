d04c747866a2171f706e47266539ab0cdbbcbcaf
Sergiy Poddubka <poseal@gmail.com>
2021-03-21 12:22:51 +0200

    add some tests

---
+++ src/test/java/ua/com/poseal/library/controllers/BookControllerTest.java
@@ -0,0 +1,98 @@
+package ua.com.poseal.library.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import ua.com.poseal.library.models.Book;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createBook() throws Exception {
+        Book testBook = mockBook();
+        String json = objectMapper.writeValueAsString(testBook);
+
+        MvcResult mvcResult =
+                mockMvc.perform(post("/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                        .andDo(print())
+                        .andExpect(status().isCreated())
+                        .andReturn();
+
+        Book bookResult = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Book.class);
+        assertThat(bookResult.getId(), notNullValue());
+        assertThat(bookResult.getName(), equalTo(testBook.getName()));
+        assertThat(bookResult.getAuthor(), equalTo(testBook.getAuthor()));
+        assertThat(bookResult.getDescription(), equalTo(testBook.getDescription()));
+        assertThat(bookResult.getPublisher(), equalTo(testBook.getPublisher()));
+        assertThat(bookResult.getYear(), equalTo(testBook.getYear()));
+        assertThat(bookResult.getIsbn(), equalTo(testBook.getIsbn()));
+    }
+
+    @Test
+    void findBook() throws Exception {
+        Book testBook = mockBook();
+        String json = objectMapper.writeValueAsString(testBook);
+
+        mockMvc.perform(post("/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        String unknownAuthor = "qwert";
+        mockMvc.perform(get("/book")
+                .param("page", "0")
+                .param("size", "1")
+                .param("query", unknownAuthor))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", empty()));
+
+        String knownAuthor = "author";
+        mockMvc.perform(get("/book")
+                .param("page", "0")
+                .param("size", "1")
+                .param("query", knownAuthor))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)));
+    }
+
+    private Book mockBook() {
+        Book testBook = new Book();
+        testBook.setName("Test name");
+        testBook.setDescription("Test description");
+        testBook.setAuthor("Test author");
+        testBook.setPublisher("Test publisher");
+        testBook.setYear(2021);
+        testBook.setIsbn("Test isbn");
+
+        return testBook;
+    }
+}
